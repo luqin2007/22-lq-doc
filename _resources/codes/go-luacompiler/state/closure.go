fdef11d4c779303c931528da9b80dc89880eb2a2
luqin2007 <lq2007lq@hotmail.com>
2024-11-26 16:26:54 +0800

    vault backup: 2024-11-26 16:26:53

--- _resources/codes/go-luacompiler/state/closure.go
+++ _resources/codes/go-luacompiler/state/closure.go
@@ -1,11 +1,21 @@
 package state
 
-import "go-luacompiler/binchunk"
+import (
+    "go-luacompiler/api"
+    "go-luacompiler/binchunk"
+)
+
+type GoFunction func(state api.LuaState) int
 
 type Closure struct {
-	proto *binchunk.Prototype
+    proto  *binchunk.Prototype
+    goFunc GoFunction
 }
 
 func newLuaClosure(proto *binchunk.Prototype) *Closure {
-	return &Closure{proto: proto}
+    return &Closure{proto: proto}
+}
+
+func newGoClosure(goFunc GoFunction) *Closure {
+    return &Closure{goFunc: goFunc}
 }
